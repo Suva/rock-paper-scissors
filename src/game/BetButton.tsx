5706b46ee61b74f53883e9fc9a8c305c628a82e4
Jaan Pullerits <clysuva@gmail.com>
2021-04-28 16:33:30 +0300

    Use interface for BetButton props

--- src/game/BetButton.tsx
+++ src/game/BetButton.tsx
@@ -3,14 +3,20 @@ import classNames from "classnames";
 import "./BetButton.scss"
 import {MAX_BETS} from "../settings";
 
-export default function BetButton(props: { bets: Gesture[], type: Gesture, onBet: Function }) {
+interface Props {
+    bets: Gesture[],
+    type: Gesture,
+    onBet: Function
+}
+
+export default function BetButton(props: Props) {
     return <button
         className={classNames({
             betButton: true,
             active: props.bets.includes(props.type),
             disabled: props.bets.length === MAX_BETS && !props.bets.includes(props.type)
         })}
-        onClick={()=>props.onBet(props.type)}>
+        onClick={() => props.onBet(props.type)}>
         {(["", "✊", "✋", "✌",])[props.type]}
     </button>
 }
